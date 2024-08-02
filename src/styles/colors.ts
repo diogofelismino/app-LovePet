48123ec29070328522dd56c6f727c2256ee2cf93
diogofelismino <94204823+diogofelismino@users.noreply.github.com>
2024-08-02 14:16:31 -0400

    Feature/Criando style de cor universal

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export const BACKGROUND_PINK = "#ED6190";
+export const BACKGROUND_GERAL = "#FCF7FA"
+export const COLOR_BUTTON = "#E5195E";
+export const BACKGROUND_INPUT = "#F2E8EB";
+export const COLOR_FONT_INPUT = "#BE91A0";
+export const BACKGROUND_HEARDER = "#DF82A1"
+export const COLOR_FONT_CARD = "#964F66";
+
+
+export const BACKGROUND_CARD_01 = "#FFF6FD";
+export const BACKGROUND_CARD_02 = "#F2E8EB"; 
+export const BACKGROUND_CARD_03 = "#F57CA5"; 
+export const BACKGROUND_CARD_04 = "#D199AB"; 
+export const BACKGROUND_CARD_05 = "#964F66"; 
+export const BACKGROUND_CARD_06 = "#9DE5B1"; 
+export const BACKGROUND_CARD_07 = "#92AA94"; 
+export const BACKGROUND_CARD_08 = "#BEEEFD"; 
+export const BACKGROUND_CARD_09 = "#71C1DA"; 
+
